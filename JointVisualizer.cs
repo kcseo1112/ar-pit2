bb48c6a7628cc6f78c29af3a94edce9be8076a3e
kangchanseo <sangchan0101@naver.com>
2026-03-12 22:35:39 +0900

    일단 허리 및 중앙 고정?

--- JointVisualizer.cs
+++ JointVisualizer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class JointVisualizer : MonoBehaviour
 {
     public GameObject jointPrefab;
@@ -13,16 +14,24 @@ public class JointVisualizer : MonoBehaviour
         }
     }
 
-    public float scale = 1f;
-
+    public float scale = 0.2f;
+    public Transform avatarHips;
 public void UpdateJoints(Vector3[] smplJoints)
 {
+    
     if (smplJoints == null || smplJoints.Length < 24)
         return;
 
+    Vector3 pelvis = smplJoints[0];
+
     for (int i = 0; i < 24; i++)
     {
-        joints[i].transform.position = smplJoints[i] * scale;
+        Vector3 pos = (smplJoints[i] - pelvis) * scale;
+
+        if (avatarHips != null)
+            joints[i].transform.position = avatarHips.position + pos;
+        else
+            joints[i].transform.position = pos;
     }
 }
 }
